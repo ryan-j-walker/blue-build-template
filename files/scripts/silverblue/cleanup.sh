a597f6bb3244c543e5ee298c516f9f27505b6b1e
Ryan Walker <ryanwalker8484@pm.me>
2026-08-30 15:42:57 -0400

    removing google and nerd fonts

--- files/scripts/silverblue/cleanup.sh
+++ files/scripts/silverblue/cleanup.sh
@@ -3,9 +3,3 @@
 set -ouex pipefail
 
 sed -Ei "s/secure_path = (.*)/secure_path = \1:\/home\/linuxbrew\/.linuxbrew\/bin/" /etc/sudoers
-
-mv /usr/share/fonts/google-fonts/Arvo/ /usr/share/fonts/arvo-fonts/
-mv /usr/share/fonts/google-fonts/Fira\ Mono/ /usr/share/fonts/fira-mono-fonts/
-mv /usr/share/fonts/google-fonts/Fira\ Sans/ /usr/share/fonts/fira-sans-fonts/
-mv /usr/share/fonts/nerd-fonts/FiraCode/ /usr/share/fonts/fira-code-nerd-fonts/
-fc-cache --system-only --really-force "/usr/share/fonts"
